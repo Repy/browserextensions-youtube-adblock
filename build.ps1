3c62bd5fa2bf78a9ad9a6d79502ba93309b3b180
Repy <gamecube02+git@gmail.com>
2024-05-11 22:07:48 +0900

    pwshでコピーも実施

--- build.ps1
+++ build.ps1
@@ -1,4 +1,8 @@
 Set-Location -Path (Split-Path -Path $MyInvocation.MyCommand.Source -Parent)
+
 Start-Process -Wait -FilePath "npm" -ArgumentList "ci"
-Start-Process -Wait -FilePath "npm" -ArgumentList "run","build"
+Start-Process -Wait -FilePath "npm" -ArgumentList "exec", "--", "tsc"
+
+Copy-Item -Path "src\*" -Destination "dist\" -Recurse
+
 Compress-Archive -Force -Path "dist\*" -DestinationPath "YTAdBlock.zip"
